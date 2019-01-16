d8e6b70a162a5a9ad15a023327fa0bc9e5ad5f6b
mezz <tehgeek@gmail.com>
2019-01-15 16:30:50 -0800

    Avoid wrapping thrown exceptions from event handlers

--- src/main/java/net/minecraftforge/eventbus/EventBus.java
+++ src/main/java/net/minecraftforge/eventbus/EventBus.java
@@ -251,7 +251,7 @@ public class EventBus implements IEventExceptionHandler, IEventBus {
         catch (Throwable throwable)
         {
             exceptionHandler.handleException(this, event, listeners, index, throwable);
-            throw new RuntimeException(throwable);
+            throw throwable;
         }
         return (event.isCancelable() ? event.isCanceled() : false);
     }
