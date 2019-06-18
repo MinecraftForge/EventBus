bc511e36530fc715453b31d5e52e4c68b3755e3c
LexManos <LexManos@gmail.com>
2019-06-18 13:47:22 -0700

    Make error friendlier when TypeResolver can not determine event type.

--- src/main/java/net/minecraftforge/eventbus/EventBus.java
+++ src/main/java/net/minecraftforge/eventbus/EventBus.java
@@ -188,6 +188,10 @@ public class EventBus implements IEventExceptionHandler, IEventBus {
     @SuppressWarnings("unchecked")
     private <T extends Event> void addListener(final EventPriority priority, final Predicate<? super T> filter, final Consumer<T> consumer) {
         final Class<T> eventClass = (Class<T>) TypeResolver.resolveRawArgument(Consumer.class, consumer.getClass());
+        if ((Class<?>)eventClass == TypeResolver.Unknown.class) {
+            LOGGER.error(EVENTBUS, "Failed to resolve handler for \"{}\"", consumer.toString());
+            throw new IllegalStateException("Failed to resolve consumer event type: " + consumer.toString());
+        }
         if (Objects.equals(eventClass, Event.class))
             LOGGER.warn(EVENTBUS,"Attempting to add a Lambda listener with computed generic type of Event. " +
                     "Are you sure this is what you meant? NOTE : there are complex lambda forms where " +
