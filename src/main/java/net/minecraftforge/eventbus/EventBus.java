58f79f7ee145e253600a9af123b733e8ba928158
tterrag <tterrag1098@gmail.com>
2019-06-23 22:13:52 -0400

    First draft super-simple event filtering API

--- src/main/java/net/minecraftforge/eventbus/EventBus.java
+++ src/main/java/net/minecraftforge/eventbus/EventBus.java
@@ -44,25 +44,29 @@ public class EventBus implements IEventExceptionHandler, IEventBus {
     private final int busID = maxID.getAndIncrement();
     private final IEventExceptionHandler exceptionHandler;
     private volatile boolean shutdown = false;
+    
+    private final Class<? extends Event> baseType;
 
     private EventBus()
     {
         ListenerList.resize(busID + 1);
         exceptionHandler = this;
         this.trackPhases = true;
+        this.baseType = Event.class;
     }
 
-    private EventBus(final IEventExceptionHandler handler, boolean trackPhase, boolean startShutdown)
+    private EventBus(final IEventExceptionHandler handler, boolean trackPhase, boolean startShutdown, Class<? extends Event> baseType)
     {
         ListenerList.resize(busID + 1);
         if (handler == null) exceptionHandler = this;
         else exceptionHandler = handler;
         this.trackPhases = trackPhase;
         this.shutdown = startShutdown;
+        this.baseType = baseType;
     }
 
     public EventBus(final BusBuilder busBuilder) {
-        this(busBuilder.getExceptionHandler(), busBuilder.getTrackPhases(), busBuilder.isStartingShutdown());
+        this(busBuilder.getExceptionHandler(), busBuilder.getTrackPhases(), busBuilder.isStartingShutdown(), busBuilder.getBaseType());
     }
 
     private void registerClass(final Class<?> clazz) {
@@ -134,6 +138,12 @@ public class EventBus implements IEventExceptionHandler, IEventBus {
                     "Method " + method + " has @SubscribeEvent annotation, " +
                             "but takes an argument that is not an Event subtype : " + eventType);
         }
+        if (baseType != Event.class && !baseType.isAssignableFrom(eventType))
+        {
+            throw new IllegalArgumentException(
+                    "Method " + method + " has @SubscribeEvent annotation, " +
+                            "but takes an argument that is not a subtype of the base type " + baseType + ": " + eventType);
+        }
 
         register(eventType, target, real);
     }
@@ -201,6 +211,10 @@ public class EventBus implements IEventExceptionHandler, IEventBus {
     }
 
     private <T extends Event> void addListener(final EventPriority priority, final Predicate<? super T> filter, final Class<T> eventClass, final Consumer<T> consumer) {
+        if (baseType != Event.class && !baseType.isAssignableFrom(eventClass)) {
+            throw new IllegalArgumentException(
+                    "Listener for event " + eventClass + " takes an argument that is not a subtype of the base type " + baseType);
+        }
         addToListeners(consumer, eventClass, e-> doCastFilter(filter, eventClass, consumer, e), priority);
     }
 
