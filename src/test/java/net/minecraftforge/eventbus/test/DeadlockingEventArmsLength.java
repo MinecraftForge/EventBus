ed8c7aef0984398bdcd754e498e2283807ec38ca
cpw <cpw+github@weeksfamily.ca>
2019-07-24 18:39:33 -0400

    How about a working deadlock test..
    Signed-off-by: cpw <cpw+github@weeksfamily.ca>

--- src/test/java/net/minecraftforge/eventbus/test/DeadlockingEventArmsLength.java
+++ src/test/java/net/minecraftforge/eventbus/test/DeadlockingEventArmsLength.java
@@ -1,11 +1,23 @@
 package net.minecraftforge.eventbus.test;
 
 import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class DeadlockingEventArmsLength {
-    public static class GrandParentEvent extends Event {}
-    public static class ParentEvent extends GrandParentEvent {}
-    public static class ChildEvent extends ParentEvent {}
-    public static class Child2Event extends ParentEvent {}
     public static class DummyEvent extends Event {}
+    public static class ParentEvent extends Event {}
+    public static class ChildEvent extends ParentEvent {
+        static {
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public static class Listener1 {
+        @SubscribeEvent
+        public static void listen(ChildEvent evt) {}
+    }
 }
--- src/test/java/net/minecraftforge/eventbus/test/DeadlockingEventTest.java
+++ src/test/java/net/minecraftforge/eventbus/test/DeadlockingEventTest.java
@@ -2,64 +2,86 @@ package net.minecraftforge.eventbus.test;
 
 import cpw.mods.modlauncher.Launcher;
 import cpw.mods.modlauncher.TransformingClassLoader;
+import net.minecraftforge.eventbus.ListenerList;
+import net.minecraftforge.eventbus.api.BusBuilder;
 import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.eventbus.api.EventListenerHelper;
+import net.minecraftforge.eventbus.api.IEventBus;
 import org.apache.logging.log4j.LogManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+import org.powermock.reflect.Whitebox;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.Random;
 import java.util.concurrent.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DeadlockingEventTest {
-    @Test
-    public void testConstructEventDeadlock() {
+    private static final boolean initializeAtClassloading = false;
+    @BeforeAll
+    static void setup() {
         System.setProperty("test.harness", "out/production/classes,out/test/classes,out/mlservice/classes,out/mlservice/resources,out/testJars/classes,build/classes/java/main,build/classes/java/mlservice,build/classes/java/test,build/classes/java/testJars,build/resources/mlservice");
         System.setProperty("test.harness.callable", "net.minecraftforge.eventbus.test.DeadlockingEventTest$Callback");
+    }
+    @RepeatedTest(50)
+    void testConstructEventDeadlock() {
         Launcher.main("--version", "1.0", "--launchTarget", "testharness");
+    }
+
+    @SuppressWarnings("unchecked")
+    @AfterEach
+    void clearBusStuff() throws IllegalAccessException {
+        final Map<Class<?>, ListenerList> listeners = (Map<Class<?>, ListenerList>) Whitebox.getField(EventListenerHelper.class, "listeners").get(null);
+        listeners.clear();
     }
 
     public static class Callback {
         public static Callable<Void> supplier() {
             return () -> {
                 final TransformingClassLoader contextClassLoader = (TransformingClassLoader) Thread.currentThread().getContextClassLoader();
+                LogManager.getLogger().info("Class Loader {}", contextClassLoader);
                 contextClassLoader.addTargetPackageFilter(s -> !(
                         s.startsWith("net.minecraftforge.eventbus.") &&
                                 !s.startsWith("net.minecraftforge.eventbus.test")));
 
-                Class.forName("net.minecraftforge.eventbus.test.DeadlockingEventArmsLength$DummyEvent", true, contextClassLoader);
-                Callable<Void> task1 = () -> {
-                    LogManager.getLogger().info("Task 1");
-                    Class.forName("net.minecraftforge.eventbus.test.DeadlockingEventArmsLength$ParentEvent", true, contextClassLoader);
-                    LogManager.getLogger().info("Task 1");
-                    return null;
-                };
+                final IEventBus bus = BusBuilder.builder().build();
                 Callable<Void> task2 = () -> {
                     LogManager.getLogger().info("Task 2");
-                    Class.forName("net.minecraftforge.eventbus.test.DeadlockingEventArmsLength$ChildEvent", true, contextClassLoader);
+                    final Class<? extends Event> clz = (Class<? extends Event>) Class.forName("net.minecraftforge.eventbus.test.DeadlockingEventArmsLength$ChildEvent", initializeAtClassloading, contextClassLoader);
+                    Thread.sleep(0, new Random().nextInt(10000));
+                    assertEquals(clz.newInstance().getListenerList(), EventListenerHelper.getListenerList(clz));
                     LogManager.getLogger().info("Task 2");
                     return null;
                 };
-                Callable<Void> task3 = () -> {
-                    LogManager.getLogger().info("Task 3");
-                    contextClassLoader.loadClass("net.minecraftforge.eventbus.test.DeadlockingEventArmsLength$Child2Event");
-                    LogManager.getLogger().info("Task 3");
+                Callable<Void> task1 = () -> {
+                    LogManager.getLogger().info("Task 1");
+                    final Class<?> clz = Class.forName("net.minecraftforge.eventbus.test.DeadlockingEventArmsLength$Listener1", initializeAtClassloading, contextClassLoader);
+                    bus.register(clz);
+                    LogManager.getLogger().info("Task 1");
                     return null;
                 };
                 final List<Future<Void>> futures;
                 try {
-                    futures = Executors.newFixedThreadPool(4).invokeAll(Arrays.asList(task1, task3, task2, task1), 1, TimeUnit.SECONDS);
+                    futures = Executors.newFixedThreadPool(2).invokeAll(Arrays.asList(task1, task2), 100, TimeUnit.SECONDS);
                 } catch (InterruptedException e) {
                     fail("Interrupted", e);
                     throw new RuntimeException();
                 }
-                futures.forEach(f -> {
-                    try {
-                        f.get();
-                    } catch (InterruptedException | ExecutionException e) {
-                        fail("error", e);
-                    }
+                assertTimeoutPreemptively(Duration.ofSeconds(1), () -> {
+                    futures.forEach(f -> {
+                        try {
+                            f.get();
+                        } catch (InterruptedException | ExecutionException e) {
+                            fail("error", e);
+                        }
+                    });
                 });
                 return null;
             };
