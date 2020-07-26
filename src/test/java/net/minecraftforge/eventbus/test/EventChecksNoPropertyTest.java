0e9d9ddfd8f73e1cf511475683a0174881f4456f
cpw <cpw+github@weeksfamily.ca>
2020-07-26 16:51:32 -0400

    Fix tests.
    Signed-off-by: cpw <cpw+github@weeksfamily.ca>

--- src/test/java/net/minecraftforge/eventbus/test/EventChecksNoPropertyTest.java
+++ src/test/java/net/minecraftforge/eventbus/test/EventChecksNoPropertyTest.java
@@ -1,33 +1,27 @@
 package net.minecraftforge.eventbus.test;
 
+import net.minecraftforge.eventbus.api.BusBuilder;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.eventbus.api.IEventBus;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import net.minecraftforge.eventbus.api.BusBuilder;
-import net.minecraftforge.eventbus.api.Event;
-import net.minecraftforge.eventbus.api.IEventBus;
+public class EventChecksNoPropertyTest {
 
-public class EventChecksTest {
-    
     public static class BaseEvent extends Event {
-        
+
         public BaseEvent() {}
     }
-    
+
     public static class OtherEvent extends Event {
-        
+
         public OtherEvent() {}
     }
-    
+
     private static final String PROP_NAME = "eventbus.checkTypesOnDispatch";
-    
-    @BeforeAll
-    public static void setup() {
-        System.setProperty(PROP_NAME, "true");
-    }
-    
+
     private static IEventBus bus() {
         return new BusBuilder().baseType(BaseEvent.class).build();
     }
@@ -38,12 +32,12 @@ public class EventChecksTest {
         Assertions.assertDoesNotThrow(() -> bus.addListener((BaseEvent e) -> {}));
         Assertions.assertDoesNotThrow(() -> bus.post(new BaseEvent()));
     }
-    
+
     @Test
     public void testInvalidType() {
         IEventBus bus = bus();
         Assertions.assertThrows(IllegalArgumentException.class, () -> bus.addListener((OtherEvent e) -> {}));
-        Assertions.assertThrows(IllegalArgumentException.class, () -> bus.post(new OtherEvent()));
+        Assertions.assertDoesNotThrow(() -> bus.post(new OtherEvent()));
     }
 
     @AfterAll
