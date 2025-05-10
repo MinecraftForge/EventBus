6858e2bcef2716adbc0bc2fe749d2c742bfa4c8e
Paint_Ninja <PaintNinja@users.noreply.github.com>
2025-05-10 14:55:08 +0100

    EventListener partial docs
    Co-Authored-By: Jonathing <me@jonathing.me>

--- src/main/java/net/minecraftforge/eventbus/api/listener/EventListener.java
+++ src/main/java/net/minecraftforge/eventbus/api/listener/EventListener.java
@@ -6,6 +6,7 @@ package net.minecraftforge.eventbus.api.listener;
 
 import net.minecraftforge.eventbus.api.bus.CancellableEventBus;
 import net.minecraftforge.eventbus.api.bus.EventBus;
+import net.minecraftforge.eventbus.api.event.characteristic.Cancellable;
 import net.minecraftforge.eventbus.internal.Event;
 import net.minecraftforge.eventbus.internal.EventListenerImpl;
 
@@ -15,19 +16,22 @@ import java.util.function.Consumer;
  * Users can retain instances of this interface to remove listeners that were previously added to the same
  * {@link EventBus}.You can obtain instances of this interface by calling any of the {@code addListener} methods
  * on an EventBus, such as {@link EventBus#addListener(Consumer)}.
- * <p>Internally, this acts as a wrapper over lambdas to give them identity, enrich debug info and to allow
- * various conversion operations to different lambda types.</p>
+ *
+ * @implNote Internally, this acts as a wrapper over lambdas to give them identity, enrich debug info and to allow
+ * various conversion operations to different lambda types.
  */
 public sealed interface EventListener permits EventListenerImpl {
     @SuppressWarnings("ClassEscapesDefinedScope") // ? can be a subtype of Event which is publicly accessible
     Class<? extends Event> eventType();
 
     /**
+     * The priority of this listener. Higher numbers are called first.
      * @see Priority
      */
     byte priority();
 
     /**
+     * Whether this listener is known to always cancel the {@link Cancellable} event when called.
      * @see CancellableEventBus#addListener(boolean, Consumer)
      */
     default boolean alwaysCancelling() {
